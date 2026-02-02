fb6ed0277a6ee7851a10fa64d427593c86e46303
eli <eliplutchok1@gmail.com>
2026-02-02 18:14:06 -0500

    e:

--- scripts/base_train.py
+++ scripts/base_train.py
@@ -99,7 +99,7 @@ else:
 # wandb logging init
 use_dummy_wandb = args.run == "dummy" or not master_process
 
-wandb_run = DummyWandb() if use_dummy_wandb else wandb.init(project="macbook-test", name=args.run, config=user_config)
+wandb_run = DummyWandb() if use_dummy_wandb else wandb.init(project="exp-nanochat", name=args.run, config=user_config)
 
 # Flash Attention status
 if HAS_FA3:
