f7fb344085689740441353bde0804dd1b91d1b0a
eli <eliplutchok1@gmail.com>
2026-02-09 13:51:38 -0500

    adjusted shs

--- runs/experiment1.sh
+++ runs/experiment1.sh
@@ -1,5 +1,4 @@
-
-# d24 model (slightly overtrained is enough to beat GPT-2 => increase data:params ratio from compute optimal 10.5 (default) to 12)
+# this is the baseline
 torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=20 --target-param-data-ratio=12 --device-batch-size=16 --run=e1
 # evaluate the model: CORE metric, BPB on train/val, and draw samples
 torchrun --standalone --nproc_per_node=8 -m scripts.base_eval -- --device-batch-size=16
--- runs/experiment2.sh
+++ runs/experiment2.sh
@@ -1,5 +1,4 @@
-
-# d24 model (slightly overtrained is enough to beat GPT-2 => increase data:params ratio from compute optimal 10.5 (default) to 12)
+# this is the experiment to test the effect of drop-loss
 torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=20 --target-param-data-ratio=12 --device-batch-size=16 --run=e2 --drop-loss-start=0.10 --drop-loss-end=0.0 --drop-loss-decay-ratio=0.5 --drop-loss-warmup-ratio=0.00
 # evaluate the model: CORE metric, BPB on train/val, and draw samples
 torchrun --standalone --nproc_per_node=8 -m scripts.base_eval -- --device-batch-size=16
