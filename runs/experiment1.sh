1f5a8ac84399408d2cfe1d79b93c008ebe936208
eli <eliplutchok1@gmail.com>
2026-02-09 16:53:19 -0500

    ablation addition

--- runs/experiment1.sh
+++ runs/experiment1.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+source .venv/bin/activate
 # this is the baseline
 torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=20 --target-param-data-ratio=12 --device-batch-size=16 --run=e1
 # evaluate the model: CORE metric, BPB on train/val, and draw samples
--- runs/experiment3.sh
+++ runs/experiment3.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+source .venv/bin/activate
 #  this is the ablation study of drop-loss
 torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=20 --target-param-data-ratio=12 --device-batch-size=16 --run=e3 --drop-loss-start=0.10 --drop-loss-end=0.0 --drop-loss-decay-ratio=0.5 --drop-loss-warmup-ratio=0.00 --drop-loss-random
 # evaluate the model: CORE metric, BPB on train/val, and draw samples
