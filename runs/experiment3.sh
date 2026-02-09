e958a023a98a6d4a1e1d1a0d3fcef38db5d726cc
eli <eliplutchok1@gmail.com>
2026-02-09 13:38:35 -0500

    added sh file

---
+++ runs/experiment3.sh
@@ -0,0 +1,4 @@
+# d24 model (slightly overtrained is enough to beat GPT-2 => increase data:params ratio from compute optimal 10.5 (default) to 12)
+torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=20 --target-param-data-ratio=12 --device-batch-size=16 --run=e3 --drop-loss-start=0.10 --drop-loss-end=0.0 --drop-loss-decay-ratio=0.5 --drop-loss-warmup-ratio=0.00 --drop-loss-random
+# evaluate the model: CORE metric, BPB on train/val, and draw samples
+torchrun --standalone --nproc_per_node=8 -m scripts.base_eval -- --device-batch-size=16
